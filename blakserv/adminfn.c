efa55f56c3b8b356cfbab46fe5ea5e9fa16dbf20
Matthew Trey <m.d.trey@gmail.com>
2016-01-05 21:39:32 -0500

    - add code to fork non-interactive startup to the background. - interactive startup option is blakserv -i (this will give you a command line interface to the server)

--- blakserv/adminfn.c
+++ blakserv/adminfn.c
@@ -1506,7 +1506,7 @@ void AdminShowBlockers(int session_id,admin_parm_type parms[],
                       int num_blak_parm,parm_node blak_parm[])
 {
    room_node *room;
-   Blocker *b;
+   BlockerNode *b;
    int row, col, finerow, finecol;
 
    room = GetRoomDataByID((int)parms[0]);
