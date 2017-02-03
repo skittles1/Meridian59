dd8a431d492a7d1f6ab9d52d3cac5f68f2c80d0e
skittles1 <tmx883@gmail.com>
2017-02-03 09:53:00 +0800

    Update splash screen handling.
    Some improvements to splash screen handling.

--- clientd3d/dibutil.c
+++ clientd3d/dibutil.c
@@ -262,6 +262,70 @@ Bool DibReadBits(file_node *f, PDIB pdib, int version)
 }
 /************************************************************************/
 /*
+ * DibOpenBitmapFile:  Opens any .bmp file, puts the color palette into
+ *   phPalette and bitmap into phBitmap.
+ *   Return True on success.
+ */
+Bool DibOpenBitmapFile(char *szFile, HBITMAP *phBitmap, HPALETTE *phPalette)
+{
+   BITMAP bm;
+
+   *phBitmap = NULL;
+   *phPalette = NULL;
+
+   // Use LoadImage() to get the image loaded into a DIBSection
+   *phBitmap = (HBITMAP)LoadImage(NULL, szFile, IMAGE_BITMAP, 0, 0,
+      LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+   if (*phBitmap == NULL)
+   {
+      return FALSE;
+   }
+
+   // Get the color depth of the DIBSection
+   GetObject(*phBitmap, sizeof(BITMAP), &bm);
+   // If the DIBSection is 256 color or less, it has a color table
+   if ((bm.bmBitsPixel * bm.bmPlanes) <= 8)
+   {
+      HDC           hMemDC;
+      HBITMAP       hOldBitmap;
+      RGBQUAD       rgb[256];
+      LPLOGPALETTE  pLogPal;
+      WORD          i;
+
+      // Create a memory DC and select the DIBSection into it
+      hMemDC = CreateCompatibleDC(NULL);
+      hOldBitmap = (HBITMAP)SelectObject(hMemDC, *phBitmap);
+      // Get the DIBSection's color table
+      GetDIBColorTable(hMemDC, 0, 256, rgb);
+      // Create a palette from the color tabl
+      pLogPal = (LOGPALETTE *)malloc(sizeof(LOGPALETTE) + (256 * sizeof(PALETTEENTRY)));
+      pLogPal->palVersion = 0x300;
+      pLogPal->palNumEntries = 256;
+      for (i = 0;i<256;i++)
+      {
+         pLogPal->palPalEntry[i].peRed = rgb[i].rgbRed;
+         pLogPal->palPalEntry[i].peGreen = rgb[i].rgbGreen;
+         pLogPal->palPalEntry[i].peBlue = rgb[i].rgbBlue;
+         pLogPal->palPalEntry[i].peFlags = 0;
+      }
+      *phPalette = CreatePalette(pLogPal);
+      // Clean up
+      free(pLogPal);
+      SelectObject(hMemDC, hOldBitmap);
+      DeleteDC(hMemDC);
+   }
+   else   // It has no color table, so use a halftone palette
+   {
+      HDC hRefDC;
+
+      hRefDC = GetDC(NULL);
+      *phPalette = CreateHalftonePalette(hRefDC);
+      ReleaseDC(NULL, hRefDC);
+   }
+   return TRUE;
+}
+/************************************************************************/
+/*
  * Free memory associated with PDIB.
  */
 void DibFree(PDIB pdib)
